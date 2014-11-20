0050368fe2aa9429e011b380ef254947a30b9c68
Daniel Bengtsson <daniel@bengtssons.info>
2014-11-20 21:01:58 +0100

    Some line drawing

--- traffic.py
+++ traffic.py
@@ -8,6 +8,14 @@ class Grid:
     def __init__(self, width, height):
         self._width = width
         self._height = height
+        self._paths = []
+
+    def add_path(self, path):
+        self._paths.append(path)
+
+    @property
+    def paths(self):
+        return self._paths
 
     def __str__(self):
         return "[{},{}]".format(self._width, self._height)
@@ -19,6 +27,14 @@ class Pos:
         self._x = x
         self._y = y
 
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
     def __str__(self):
         return "({}, {})".format(self._x, self._y)
 
@@ -26,9 +42,17 @@ class Pos:
 class Path:
     """A path in the grid"""
     def __init__(self, _grid, start, end):
-        self._grid = _grid
         self._start = start
         self._end = end
+        grid.add_path(self)
+
+    @property
+    def start(self):
+        return self._start
+
+    @property
+    def end(self):
+        return self._end
 
     def __str__(self):
         return "[{} -> {}]".format(self._start, self._end)
@@ -63,15 +87,30 @@ class CursesRenderer(Renderer):
         self._scr.keypad(0)
         curses.endwin()
 
+    def line(self, path):
+        s = path.start
+        e = path.end
+        if (e.y-s.y) < (e.x-s.x):
+            d = (e.y-s.y)/(e.x-s.x)
+            for x in range(s.x, e.x):
+                y = s.y+(d*(x-s.x))
+                self._scr.addch(int(y), int(x), '¤')
+        else:
+            for y in range(s.y, e.y):
+                d = (e.x-s.x)/(e.y-s.y)
+                x = s.x+(d*(y-s.y))
+                self._scr.addch(int(y), int(x), '¤')
+
     def render(self):
         self._scr.addstr(str(self._grid))
+        for p in self._grid.paths:
+            self.line(p)
         self._scr.getch()
 
 
 if __name__ == '__main__':
     grid = Grid(100, 100)
-    p1 = Path(grid, Pos(10, 10), Pos(10, 20))
-    print(p1)
+    p1 = Path(grid, Pos(5, 10), Pos(24, 18))
 
     with CursesRenderer(grid) as renderer:
         renderer.render()
